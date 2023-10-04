0f4c29b78593fa3f4425134efa049bb934275332
rickyca10 <anhcv@icitech.net>
2023-10-04 16:38:31 +0700

    Fix migration and seeder

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace IBoot\Core\Database\Seeders;
 
 use IBoot\Core\app\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -16,9 +17,10 @@ class UserSeeder extends Seeder
     {
         User::create([
             'email' => 'admin@icitech.net',
+            'name' => 'Admin',
             'status' => User::ACTIVATED,
             'username' => 'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
         ]);
     }
 }
